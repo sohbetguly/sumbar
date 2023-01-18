1bc7cdb83c1ccb43d5e2246376146b8d0fcd279c
Sohbet Danegulyyew <sobtware@gmail.com>
2023-01-18 22:55:59 +0430

    changed

--- src/layout/ProductCard.jsx
+++ src/layout/ProductCard.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import { AiFillHeart, AiOutlineHeart, AiOutlineZoomIn } from "react-icons/ai";
 import classes from "./ProductCard.module.css";
 
 function ProductCard({ image, price, caption }) {
   return (
     <div className="border p-2">
       <div>
-        <div className="d-inline-block page-link popover-arrow px-2 border rounded">
-          <Form.Check type="checkbox" id={image}>
-            <Form.Check.Input type="checkbox" isInvalid />
-            <Form.Check.Label
-              style={{ cursor: "pointer" }}
-              className="cursor-pointer text-dark"
-              htmlFor={image}
-            >
-              Deňeşdirmek
-            </Form.Check.Label>
-          </Form.Check>
+        <div className="d-inline-block position-relative top-0 end-100">
+          <div className="d-inline-block page-link popover-arrow py-1 px-2 border rounded ">
+            <Form.Check type="checkbox" id={image}>
+              <Form.Check.Input type="checkbox" isInvalid className="" />
+              <Form.Check.Label
+                style={{ cursor: "pointer" }}
+                className="cursor-pointer text-dark"
+                htmlFor={image}
+              >
+                Deňeşdirmek
+              </Form.Check.Label>
+            </Form.Check>
+          </div>
         </div>
+        <AiOutlineZoomIn style={{ cursor: "pointer" }} className="mx-1 fs-5" />
+        <AiOutlineHeart className="text-danger fs-5" />
+
         <a href="#">
           <img src={image} className="img-fluid " />
         </a>
@@ -27,7 +33,7 @@ function ProductCard({ image, price, caption }) {
           {caption}
         </a>
         <div className="mt-auto">
-          <hr className="text-danger w-50 my-2" />
+          <hr className="text-danger w-50 my-2 " />
 
           <div className="h6">
             {price} <small>TMT</small>
